838b226495fef5ce9f7307955b1f55e68512be10
David <davibloch@googlemail.com>
2020-04-29 19:52:29 +0200

    First Working Version
    First Time got Snake Working

--- SnaKI.py
+++ SnaKI.py
@@ -192,7 +192,7 @@ def main():
  
         for x in range(len(s.body)):
             if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
-                   print("Score: ", len(s.body) )
+                print("Score: ", len(s.body) )
                 message_box("You Lost!", "Play again...")
                 s.reset((10,10))
                 break
